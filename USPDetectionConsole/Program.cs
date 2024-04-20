68d515a579d6171bddbe5b26fbbd0c437a39e007
Terry Holmes <tchaser3@outlook.com>
2024-04-20 08:36:58 -0400

    Add project files.

---
+++ USPDetectionConsole/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace USPDetectionConsole
+{
+    
+    internal class Program
+    {
+        private void DeviceInsertedEvent(object sender, EventArrivedEventArgs e)
+        {
+            ManagementBaseObject instance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+            foreach (var property in instance.Properties)
+            {
+                Console.WriteLine(property.Name + " = " + property.Value);
+            }
+        }
+
+        private void DeviceRemovedEvent(object sender, EventArrivedEventArgs e)
+        {
+            ManagementBaseObject instance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+            foreach (var property in instance.Properties)
+            {
+                Console.WriteLine(property.Name + " = " + property.Value);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var watcher = new ManagementEventWatcher();
+            var query = new WqlEventQuery("SELECT * FROM Win32_DeviceChangeEvent WHERE EventType = 2");
+            watcher.EventArrived += new EventArrivedEventHandler(watcher_EventArrived);
+            watcher.Query = query;
+            watcher.Start();
+        }
+        
+    }
+}
